35e7b5c22745ac98cfebcd7dbcd1d3499e65a20e
Ando Rakotomanana <andorak@outlook.fr>
2021-01-27 15:58:02 +0300

    admin security system commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,51 +58,20 @@ class User implements UserInterface, Serializable
         return $this;
     }
 
-    /**
-     * Returns the roles granted to the user.
-     *
-     *  public function getRoles()
-     *  {
-     *      return array('ROLE_USER')
-     *  }
-     *
-     * Alternatively, the roles might be stored on a 'roles' property, and populated in any
-     * number of different ways when the user object is created.
-     *
-     * @return string[] (Role|string)[] The user roles
-     */
     public function getRoles(): array
     {
         return ['ROLE_ADMIN'];
     }
 
-    /**
-     * Returns the salt that was originally used to encode the password.
-     * This can return null if the password was not encoded using a salt.
-     *
-     * @return string|null The salt
-     */
     public function getSalt(): ?string
     {
         return null;
     }
 
-    /**
-     * Removes sensitive data from the user.
-     *
-     * This is important if, at any given point, sensitive information like
-     * the plain-text password is stored on this object.
-     */
     public function eraseCredentials()
     {
     }
 
-    /**
-     * String representation of object
-     * @link https://php.net/manual/en/serializable.serialize.php
-     * @return string the string representation of the object or null
-     * @since 5.1.0
-     */
     public function serialize(): string
     {
         return serialize([
@@ -112,15 +81,6 @@ class User implements UserInterface, Serializable
         ]);
     }
 
-    /**
-     * Constructs the object
-     * @link https://php.net/manual/en/serializable.unserialize.php
-     * @param string $serialized <p>
-     * The string representation of the object.
-     * </p>
-     * @return void
-     * @since 5.1.0
-     */
     public function unserialize($serialized)
     {
         list(
